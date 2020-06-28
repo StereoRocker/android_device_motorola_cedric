5e3487c83d0d43fd9927818c99348592ed80d1cd
StereoRocker <technpizza@gmail.com>
2020-06-28 17:15:26 +0100

    device.mk - corrected incorrect bindmounts

--- device.mk
+++ device.mk
@@ -286,5 +286,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 # Ubuntu Touch
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/ubuntu/lib/udev/rules.d/70-android.rules:system/halium/lib/udev/rules.d/70-android.rules \
-	$(LOCAL_PATH)/ubuntu/etc/ubuntu-touch-session.d/android.conf:system/halium/etc/ubuntu-touch-session.d/cedric.conf \
+	$(LOCAL_PATH)/ubuntu/etc/ubuntu-touch-session.d/android.conf:system/halium/etc/ubuntu-touch-session.d/android.conf \
 	$(LOCAL_PATH)/ubuntu/etc/modules:system/halium/etc/modules
