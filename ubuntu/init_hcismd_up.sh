9776385c4c72351dc756d221a7bc7f77f558d5a8
StereoRocker <technpizza@gmail.com>
2020-06-28 18:41:25 +0100

    ubports: simplified init_hcismd_up.sh and made executable

--- ubuntu/init_hcismd_up.sh
+++ ubuntu/init_hcismd_up.sh
@@ -1,19 +1,3 @@
 #!/bin/sh
 
-MAXTRIES=15
-
-export LD_LIBRARY_PATH=/vendor/lib64:/system/lib64:/vendor/lib:/system/lib
-
-#Wifi enabler
-j=1
-while [ ! $j -gt $MAXTRIES ]  ; do
-	insmod /system/lib/modules/wlan.ko
-    echo 1 > /dev/wcnss_wlan
-    echo sta > /sys/module/wlan/parameters/fwpath
-    
-    if [ "$?" -ne "0" ]; then
-      sleep 1
-    fi
-    
-    j=$((j + 1))
-done
+insmod /lib/modules/wlan.ko
